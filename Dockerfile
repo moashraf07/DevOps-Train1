27931b83944a1bd95c30b545a26f38cdd3f3e15a
Mohamed Ashraf Mostafa <mammgmm44@gmail.com>
2026-08-10 05:56:33 +0000

    Apply multi staging docker file and test multi environments.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,32 @@
-FROM node
+  
+FROM node as base
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
+
+FROM base as development
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
+
+FROM base as production
 
 WORKDIR /app
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<!DOCTYPE html><html><head><meta charset="utf-8"><title>My Page</title></head><body><h1>Hello My name Mohamed</h1></body></html>');
+  res.end('<!DOCTYPE html><html><head><meta charset="utf-8"><title>My Page</title></head><body><h1>Hello My name Mohamed enn</h1></body></html>');
 });
 
 server.listen(port, () => {
